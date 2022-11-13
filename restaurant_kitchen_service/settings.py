8104f5c69eda35b40b22fc7dfb76709b8449a835
Pavlo Kapitanets <aixpavlik"gmail.com>
2022-11-13 19:04:58 +0200

    Update settings & requirements.txt

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-2*^i=yr_=wvn*--og)d3)s9xg!(+*f)&56vp8ij2@ixcm91s@t"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-2*^i=yr_=wvn*--og)d3)s9xg!(+*f)&56vp8ij2@ixcm91s@t")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = []
 
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = "restaurant.Cook"
 
-LOGIN_REDIRECT_URL = "/restaurant/"
+LOGIN_REDIRECT_URL = "/"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
